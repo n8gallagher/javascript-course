65350f7d41b4c14fbacdf958e98e69f123421d18
nateg <n8gallagher@gmail.com>
2020-12-28 16:20:07 -0800

    First DOM assignment

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,16 @@
+// document.title = "Assignment solved!"
+let title = document.querySelector('title')
+// let title = document.querySelector(title)
+title.text = "Assignment solved!";
+// console.log(title);
+
+let h1 = document.querySelector('h1');
+console.log(h1)
+h1.innerHTML = "Assignment solved!";
+
+let task1 = document.querySelector('li');
+let task1Again = document.getElementById('task-1')
+task1Again.style.backgroundColor = '#000000';
+task1.style.color = 'white';
+
+
